ae4ba8c22af8f1037b5a918ff69e2b6547894827
witchplay <moysesperez2013@gmail.com>
2022-04-28 08:43:01 -0300

    1.0.0

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask , render_template
+
+import os
+
+
+app= Flask(__name__)
+
+_port = os.environ.get('PORT', 5000)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=_port)
